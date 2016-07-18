7266ab39bdc948be4988e77b94eecd6f19a47edf
uishnik <uishnik@yandex.ru>
2016-07-18 16:39:31 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pycryptopro',
+    version='0.0.5',
+    description='',
+    url='https://github.com/uishnik/py-cryptopro',
+    author='uishnik',
+    license='MIT'
+)
